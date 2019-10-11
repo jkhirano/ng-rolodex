b2c5dfa1b153cc92471a8eac0168c5b1c068cd2c
Janelle K Hirano <jaahnelle@gmail.com>
2019-10-10 21:56:06 -1000

    create User model

---
+++ server/database/models/User.js
@@ -0,0 +1,12 @@
+const bookshelf = require("../bookshelf");
+
+class User extends bookshelf.Model {
+  get tableName() {
+    return "users";
+  }
+  get hasTimestamps() {
+    return true;
+  }
+}
+
+module.exports = bookshelf.Model("User", User);
