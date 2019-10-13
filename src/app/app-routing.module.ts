7c7f847db4b2e61ed6e91c5bcc6606619b382390
Janelle K Hirano <jaahnelle@gmail.com>
2019-10-12 17:40:06 -1000

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
+import { ContactsComponent } from "./pages/contacts/contacts.component";
+import { CreateComponent } from "./pages/create/create.component";
+import { ProfileComponent } from "./pages/profile/profile.component";
+import { NotFoundComponent } from "./pages/notFound/notFound.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "contacts", component: ContactsComponent },
+  { path: "create", component: CreateComponent },
+  { path: "profile", component: ProfileComponent },
+  { path: "404", component: NotFoundComponent },
+  { path: "**", redirectTo: "404", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,22 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app/app.component";
 import { HomeComponent } from "./pages/home/home.component";
+import { ContactsComponent } from "./pages/contacts/contacts.component";
+import { CreateComponent } from "./pages/create/create.component";
+import { ProfileComponent } from "./pages/profile/profile.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { NotFoundComponent } from "./pages/notFound/notFound.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactsComponent,
+    CreateComponent,
+    ProfileComponent,
+    HeaderComponent,
+    NotFoundComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
